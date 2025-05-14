4749b25388fe4372895ad3a99996bcff4d92ed26
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-05-14 04:21:16 -0700

    Extends the review class with BaseModel.

--- UberReviewService/src/main/java/com/zeroo8/UberReviewService/models/Review.java
+++ UberReviewService/src/main/java/com/zeroo8/UberReviewService/models/Review.java
@@ -2,31 +2,30 @@ package com.zeroo8.UberReviewService.models;
 
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "bookingreview")
-public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long  id;
+public class Review extends BaseModel {
+
 
     @Column(nullable = false)
     String content;
     Double rating;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    Date createdAt;
+    public String toString(){
+        return "Review " + this.content + " " + this.rating + " " + this.createdAt;
+    }
 
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    Date updatedAt;
 
 }
